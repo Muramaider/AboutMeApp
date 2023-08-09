5f26008d24a1f97b9b841e517668cebe73dfc23f
Alexey <vinogradov.al.kon@gmail.com>
2023-08-09 17:18:40 +0300

    Adding Extension for Gradient method and some constants

---
+++ AboutMeApp/Constants/ColorsForGradient.swift
@@ -0,0 +1,26 @@
+//
+//  ColorsForGradient.swift
+//  AboutMeApp
+//
+//  Created by Aleksey Vinogradov on 09.08.2023.
+//
+
+import UIKit
+
+struct Constants {
+    
+    // MARK: - Colors for gradient
+    static let primaryColor = UIColor(
+        red: 210/255,
+        green: 109/255,
+        blue: 128/255,
+        alpha: 1
+    )
+    static let secondaryColor = UIColor(
+        red: 107/255,
+        green: 148/255,
+        blue: 230/255,
+        alpha: 1
+    )
+}
+
---
+++ AboutMeApp/Extensions/GradientBGColor.swift
@@ -0,0 +1,21 @@
+//
+//  UIView.swift
+//  AboutMeApp
+//
+//  Created by Aleksey Vinogradov on 09.08.2023.
+//
+
+import UIKit
+
+// MARK: - Set background color
+extension UIView {
+    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
+    }
+}
